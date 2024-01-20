de7df6765e760994fdc7141cf2d755c519b427b7
clevrmick <clevrmick@gmail.com>
2024-01-21 00:27:26 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to React Class Based Component</h1>
+      </div>
+    );
+  }
+}
