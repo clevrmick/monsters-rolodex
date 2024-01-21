c88d68b3e619c6061ec96909925ff4c466362216
clevrmick <clevrmick@gmail.com>
2024-01-21 12:39:44 +0100

    completed the fetching  of monsters and displayed them in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,31 @@ import { Component } from "react";
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        console.log(users);
+        this.setState(() => {
+          return { monsters: users };
+        });
+      });
   }
 
   render() {
     return (
       <div>
         <h1>Welcome to React Class Based Component</h1>
+        {this.state.monsters.map((monster) => {
+          return <h3 key={monster.id}>{monster.name}</h3>;
+        })}
       </div>
     );
   }
